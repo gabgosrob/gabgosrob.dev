37afcae63da5d6e6b4dc4560c85b8941e4b6da8d
Gabriel Gosselin Roberge <gabrielgosrob@gmail.com>
2022-01-11 12:22:44 -0500

    implement auto theme saving + clean up ts code

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,28 @@
-function addListeners() {
-    var lightSwitch = document.getElementById("light-switch");
-    lightSwitch.addEventListener("click", toggleLightSwitch);
-}
-function toggleLightSwitch() {
-    var lightSwitch = document.getElementById("light-switch");
-    if (lightSwitch.getAttribute("src") == "images/on.png") {
-        lightSwitch.setAttribute("src", "images/off.png");
-        setDarkMode();
+var THEME_KEY = "theme";
+var Themes;
+(function (Themes) {
+    Themes["light"] = "light";
+    Themes["dark"] = "dark";
+})(Themes || (Themes = {}));
+function setInitialTheme() {
+    var theme = localStorage.getItem(THEME_KEY);
+    if (theme == null) {
+        localStorage.setItem(THEME_KEY, Themes.light);
+        setLightTheme();
+    }
+    else if (theme == Themes.light) {
+        setLightTheme();
     }
     else {
-        lightSwitch.setAttribute("src", "images/on.png");
-        setLightMode();
+        setDarkTheme();
     }
 }
-function setDarkMode() {
+function setDarkTheme() {
+    localStorage.setItem(THEME_KEY, Themes.dark);
+    var lightSwitch = document.getElementById("light-switch");
     var body = document.querySelector("body");
     var cards = Array.from(document.querySelectorAll(".project-card"));
+    lightSwitch.setAttribute("src", "images/off.png");
     body.style.backgroundColor = "rgb(30, 30, 30)";
     body.style.color = "white";
     cards.forEach(function (card) {
@@ -23,9 +30,12 @@ function setDarkMode() {
         card.querySelector("a").style.backgroundColor = "rgb(35, 35, 35)";
     });
 }
-function setLightMode() {
+function setLightTheme() {
+    localStorage.setItem(THEME_KEY, Themes.light);
+    var lightSwitch = document.getElementById("light-switch");
     var body = document.querySelector("body");
     var cards = Array.from(document.querySelectorAll(".project-card"));
+    lightSwitch.setAttribute("src", "images/on.png");
     body.style.backgroundColor = "rgb(238, 253, 255)";
     body.style.color = "black";
     cards.forEach(function (card) {
@@ -33,4 +43,18 @@ function setLightMode() {
         card.querySelector("a").style.backgroundColor = "white";
     });
 }
+function addListeners() {
+    var lightSwitch = document.getElementById("light-switch");
+    lightSwitch.addEventListener("click", toggleTheme);
+}
+function toggleTheme() {
+    var lightSwitch = document.getElementById("light-switch");
+    if (localStorage.getItem(THEME_KEY) == Themes.dark) {
+        setLightTheme();
+    }
+    else {
+        setDarkTheme();
+    }
+}
+setInitialTheme();
 addListeners();
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,23 +1,30 @@
-function addListeners(): void {
-    const lightSwitch = document.getElementById("light-switch");
-    lightSwitch.addEventListener("click", toggleLightSwitch);
+const THEME_KEY = "theme"
+enum Themes {
+    light = "light",
+    dark = "dark"
 }
 
-function toggleLightSwitch(): void {
-    const lightSwitch = document.getElementById("light-switch");
-    if (lightSwitch.getAttribute("src") == "images/on.png") {
-        lightSwitch.setAttribute("src", "images/off.png")
-        setDarkMode();
+function setInitialTheme(): void {
+    const theme = localStorage.getItem(THEME_KEY);
+    if (theme == null) {
+        localStorage.setItem(THEME_KEY, Themes.light);
+        setLightTheme();
+    }
+    else if (theme == Themes.light) {
+        setLightTheme();
     }
     else {
-        lightSwitch.setAttribute("src", "images/on.png")
-        setLightMode();
+        setDarkTheme();
     }
 }
 
-function setDarkMode(): void {
+function setDarkTheme(): void {
+    localStorage.setItem(THEME_KEY, Themes.dark);
+    const lightSwitch = document.getElementById("light-switch");
     const body: HTMLElement = document.querySelector("body");
     const cards: Array<HTMLElement> = Array.from(document.querySelectorAll(".project-card"));
+    
+    lightSwitch.setAttribute("src", "images/off.png");
     body.style.backgroundColor = "rgb(30, 30, 30)";
     body.style.color = "white";
     cards.forEach(card => {
@@ -26,9 +33,13 @@ function setDarkMode(): void {
     });
 }
 
-function setLightMode(): void {
+function setLightTheme(): void {
+    localStorage.setItem(THEME_KEY, Themes.light);
+    const lightSwitch = document.getElementById("light-switch");
     const body: HTMLElement = document.querySelector("body");
     const cards: Array<HTMLElement> = Array.from(document.querySelectorAll(".project-card"));
+    
+    lightSwitch.setAttribute("src", "images/on.png");
     body.style.backgroundColor = "rgb(238, 253, 255)";
     body.style.color = "black";
     cards.forEach(card => {
@@ -37,4 +48,20 @@ function setLightMode(): void {
     });
 }
 
+function addListeners(): void {
+    const lightSwitch = document.getElementById("light-switch");
+    lightSwitch.addEventListener("click", toggleTheme);
+}
+
+function toggleTheme(): void {
+    const lightSwitch = document.getElementById("light-switch");
+    if (localStorage.getItem(THEME_KEY) == Themes.dark) {
+        setLightTheme();
+    }
+    else {
+        setDarkTheme();
+    }
+}
+
+setInitialTheme();
 addListeners();
